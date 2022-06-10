e5bc694abcb0d7356d5fc8b6036cda5f30a4987a
Happiness-James <gesarehjames@gmail.com>
2022-06-10 20:20:38 +0300

    changes deposit

--- bank.py
+++ bank.py
@@ -1,20 +1,37 @@
-class Bank:
+class Account:
     def __init__(self,accountname,accountnumber):
         self.accountname = accountname
         self.accountnumber = accountnumber
         self.balance = 0  
+        self.deposits = []
+        self.withdrawals = []
     def deposit(self,amount):
         if amount<=0:
             return f"Deposit must be positive"
         else: 
-            self.balance+=amount   
-            return f"Hello {self.accountname} Your balance is {self.balance}"    
+            self.balance+=amount
+            self.deposits.append(amount)
+            print(f"Hello {self.accountname} Your balance is {self.balance} you have deposited {self.deposits}")   
+                    
+
     def withdraw(self,amount):
+        self.transaction_fee = 100
         if amount>self.balance:
             return f"Hello {self.accountname} Your balance is {self.balance} you can't withdraw {amount}"
         elif amount<=0:
             return f"Hello {self.accountname} The amount should be positive" 
         else:       
-            self.balance-=amount
-            return f"Hello, {self.accountname} You have withdraw {amount} and your new balance is {self.balance}"       
+            self.balance-=amount+self.transaction_fee
+            self.withdrawals.append(amount)
+            return f"Hello, {self.accountname} You have withdraw {amount} with a {self.transaction_fee}and your new balance is {self.balance}"
+    def deposits_statement(self):
+        for i in self.deposits:
+            print(i,end="\n")   
+
+    def withdrawals_statement(self):
+        for x in self.withdrawals:
+            print(x, end="\n")  
+    def current_balance(self):
+        return self.balance         
+
            
