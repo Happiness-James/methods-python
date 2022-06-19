8a9f9fc9087d87873a807e4b7ecd247792f1da26
Happiness-James <gesarehjames@gmail.com>
2022-06-19 15:35:16 +0300

    changes loans

--- bank.py
+++ bank.py
@@ -1,37 +1,147 @@
+from datetime import datetime
+from itertools import count
 class Account:
     def __init__(self,accountname,accountnumber):
         self.accountname = accountname
         self.accountnumber = accountnumber
-        self.balance = 0  
+        self.balance = 0 
         self.deposits = []
         self.withdrawals = []
+        self.transaction_fee = 100
+        self.loan = 0
+        self.statement = []
+    
     def deposit(self,amount):
         if amount<=0:
             return f"Deposit must be positive"
         else: 
             self.balance+=amount
-            self.deposits.append(amount)
-            print(f"Hello {self.accountname} Your balance is {self.balance} you have deposited {self.deposits}")   
+            now = datetime.now()
+            transaction={
+                "amount":amount,
+                "time":now,
+                "Narration":"you have deposited"
+            }  
+            self.deposits.append(amount)   
+            self.statement.append(transaction)
+            return f"Hello {self.accountname} Your balance is {self.balance} your deposits is {self.deposits}"  
                     
 
     def withdraw(self,amount):
-        self.transaction_fee = 100
-        if amount>self.balance:
+        if amount+self.transaction_fee>self.balance:
             return f"Hello {self.accountname} Your balance is {self.balance} you can't withdraw {amount}"
         elif amount<=0:
             return f"Hello {self.accountname} The amount should be positive" 
         else:       
             self.balance-=amount+self.transaction_fee
+            now = datetime.now()
+            transaction={
+                "amount":amount,
+                "time":now,
+                "Narration":"you have withdrawn"
+            }
             self.withdrawals.append(amount)
-            return f"Hello, {self.accountname} You have withdraw {amount} with a {self.transaction_fee}and your new balance is {self.balance}"
+            self.statement.append(transaction)
+            return f"Hello, {self.accountname} your new balance is {self.balance} and your withdrawals are {self.withdrawals} {self.statement}"
+
+    def borrow(self,amount):
+            count = sum(self.deposits)
+            limit = count*(1/3)
+            interest=amount*0.03
+            if amount<=100:
+                return "Loan must be more than 100"  
+            elif self.loan>0:
+                return f"Loan denied, kindly repay your current loan of {self.loan}"
+            elif len(self.deposits)<10:
+                return "your deposits must be at least more than 10"
+
+            elif amount>=limit:
+                return f"You have reached your limit, your loan of {amount} is denied "
+  
+            elif amount>=self.balance:
+                return f"Dear customer you can't borrow that money is lower than a limit of" 
+            else:
+                 self.loan+=amount
+                 self.loan+=interest
+                 now= datetime.now()
+                 transaction={
+                    "amount":amount,
+                    "time":now,
+                    "Narration":"you have borrowed"
+                }
+                 self.statement.append(transaction)
+                 return f"Hello {self.accountname} your loan of {amount} has been approved and your interest is {interest} you will pay back {self.loan}" 
+    
+    def repay(self,amount):
+    
+        if amount<0:
+            return "Dear customer your payment is too low"
+        elif amount<=self.loan:
+            paid = self.loan - amount
+            return f"Dear customer you have  paid {amount} and your loan balance is {paid} "
+        else:
+            payment=amount-self.loan
+            self.balance+=payment
+
+            now= datetime.now()
+            transaction={
+                "amount":amount,
+                "time":now,
+                "Narration":"you have repaid your loan"
+            }
+            self.statement.append(transaction)
+
+            return f"Dear customer {self.accountname} you have fully paid your loan and the overpay of {payment} is added on your account, your new balance is {self.balance}"
+    
+    def transfer(self,account_instance,amount):
+
+        Total= amount + self.transaction_fee
+        if amount<0:
+            return f"Dear customer {self.accountname} your amount is too low"
+        elif Total>self.balance:
+            return f"Dear customer {self.accountname} you balance is {self.balance} and you need atleast {Total}"
+        else:
+            self.balance-=Total
+            return f"Dear customer you  have sent {amount} to {account_instance} your current balance is {self.balance}"                       
+
     def deposits_statement(self):
-        for i in self.deposits:
-            print(i,end="\n")   
+        for deposit in self.statement:
+            return deposit   
 
     def withdrawals_statement(self):
-        for x in self.withdrawals:
-            print(x, end="\n")  
+        for withdrawal in self.statement:
+            return withdrawal  
     def current_balance(self):
-        return self.balance         
+        return self.balance   
+    def full_statement(self):
+        for transaction in self.statement:
+            amount = transaction["amount"]
+            Narration= transaction["Narration"]
+            time= transaction["time"]
+            date= time.strftime("%x %X")
+            print(f"{date}: {Narration} {amount}")
+            
+account = Account(accountname="Happiness",accountnumber=58943)
+print(account.deposit(500))
+print(account.deposit(500))
+# print(account.deposit(200))
+# print(account.deposit(200))
+# print(account.deposit(200))
+# print(account.deposit(200))
+# print(account.deposit(200))
+# print(account.deposit(1000))
+# print(account.deposit(1000))
+print(account.deposit(1000))
+print(account.deposit(1000))
+print(account.withdraw(200))
+# print(account.withdraw(1100))
+#print(account.full_statement())
+# print(account.borrow(3000))
+# print(account.repay(5000))
+print(account.transfer("Gesare",2000))
+# print(account.transfer("Gesare",1000))
+
+
+
 
            
